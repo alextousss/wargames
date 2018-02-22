e1d38bd1ce4b60b9a0e484eb08530e4ceb9731e0
alex <belzebalex@users.noreply.github.com>
2018-02-22 19:25:09 +0100

    add max and min values to moves

--- fightingentity.py
+++ fightingentity.py
@@ -22,7 +22,15 @@ class FightingEntity:
     def moveCartesian(self,x,y):
         self.position_x += x
         self.position_y += y
+
     def move(self, x, y):
+        max_val = 1
+        min_val = -1
+        if( x > max_val): x = max_val
+        elif( x < min_val): x = min_val
+        if( y > max_val): y = max_val
+        elif( y <-min_val): y = min_val
+
         angle = math.radians(self.angle)
         self.position_x +=  math.cos(angle) * y
         self.position_y += -math.sin(angle) * y
