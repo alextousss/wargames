93d03c395688f82f18cfc02e7756cf35d15bd6a3
Alex Toussaint <belzebalex@users.noreply.github.com>
2018-03-06 21:44:29 +0100

    now able to run on environnements without pygame !

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from simulation import DarwinSelection
-from display import Display
 import sys
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
+        from simulation import DarwinSelection
+
         sel = DarwinSelection()
         sel.run()
 
     elif sys.argv[1] == "-display":
+        from display import Display
+
         dis = Display()
         dis.run()
