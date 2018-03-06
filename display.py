16c76e944f12095ad98a7045577d06758a17989b
Alex Toussaint <belzebalex@users.noreply.github.com>
2018-03-06 21:41:02 +0100

    display now fixed and working !

--- display.py
+++ display.py
@@ -26,21 +26,21 @@ class Display:
         with open('file', 'rb') as f:
             self.soldiers = pickle.load(f)
         self.sim = Simulation()
-        self.time_start = time.time()
+        self.last_frame_time = time.time()
         self.framerate = 150
 
     def run(self):
         for sol1 in self.soldiers:
             for sol2 in self.soldiers:
                 if sol1 is not sol2:
+                    self.sim = Simulation()
                     self.sim.giveSoldiers([sol1, sol2])
                     stop = False
                     while True and not stop:
-                        time_start = time.time()
 
 
                         self.sim.update()
-
+                        stop = self.sim.stop
                         self.draw(self.sim.soldiers, self.sim.bullets)
                         for event in pygame.event.get():
                             if event.type == QUIT:
@@ -53,6 +53,9 @@ class Display:
 
 
     def draw(self, soldiers, bullets):
+        while time.time() - self.last_frame_time < 1/self.framerate:
+            pass
+        self.last_frame_time = time.time()
         self.screen.fill([0, 0, 0])
         for el in bullets:
             pygame.draw.line(self.screen, [255,0,0], (el.pos.x, el.pos.y), (el.pos.x + el.move.x, el.pos.y + el.move.y), 5)
